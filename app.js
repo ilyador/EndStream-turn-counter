99d10c7c5df0a59d7d1001effdaa3fbc27556a39
ilyador <whitestar.i8@gmail.com>
2015-10-11 18:51:17 +0300

    basic turn counting logic

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
 angular.module('esCounter', []).controller('boardController', function($scope) {
 
   var streamStructure = {
-
     "1800": {
       counting: false,
       turns: 2,
-      cards: 5
+      cards: 5,
+      score: 2
     },
     "1900": {
       counting: false,
       turns: 3,
-      cards: 4
+      cards: 4,
+      score: 3
     },
     "2000": {
       counting: false,
       turns: 4,
-      cards: 3
+      cards: 3,
+      score: 4
     },
     "2100": {
       counting: false,
       turns: 5,
-      cards: 2
+      cards: 2,
+      score: 6
     },
     "2200": {
       counting: false,
       turns: 10,
-      cards: 1
+      cards: 1,
+      score: 12
     }
   };
 
+  $scope.currentPlayer = "player1";
+
   $scope.board = {
-    player1: _.merge({}, streamStructure),
-    player2: _.merge({}, streamStructure)
+    player1: {
+      stream: _.merge({}, streamStructure),
+      score: 0
+    },
+    player2: {
+      stream: _.merge({}, streamStructure),
+      score: 0
+    }
   };
 
   $scope.agentDisintegrating = function (epoch, player) {
-    console.log(epoch, player);
+    if ($scope.currentPlayer !== player) return;
 
-    var influencedTurnpoint = $scope.board[player][epoch];
+    var influencedTurnpoint = $scope.board[player].stream[epoch];
+    influencedTurnpoint.counting = !influencedTurnpoint.counting;
+  };
 
-    influencedTurnpoint.turns -= 1;
+  $scope.endTurn = function () {
+    var player = $scope.currentPlayer;
+    var stream = $scope.board[player].stream;
+
+    _.forOwn(stream, function(turnpoint, epoch) {
+      if(turnpoint.counting && turnpoint.turns > 0) {
+        turnpoint.turns -= 1;
+      } else {
+        turnpoint.turns = streamStructure[epoch].turns;
+      }
+    });
+
+    $scope.currentPlayer = (player === "player1") ? "player2":"player1"
   }
 });
