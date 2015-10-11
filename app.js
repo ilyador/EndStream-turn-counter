cb81eb5af88f16945ab440378cd916cec0bb85df
ilyador <whitestar.i8@gmail.com>
2015-10-11 12:00:12 +0300

    switcher ng-repeat to key:value

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 angular.module('esCounter', []).controller('boardController', function($scope) {
 
-  var streamStructure = [
-    {
-      epoch: "1800",
+  var streamStructure = {
+
+    "1800": {
+      counting: false,
       turns: 2,
-      counting: false,
       cards: 5
-    },{
-      epoch: "1900",
+    },
+    "1900": {
+      counting: false,
       turns: 3,
-      counting: false,
       cards: 4
-    },{
-      epoch: "2000",
+    },
+    "2000": {
+      counting: false,
       turns: 4,
-      counting: false,
       cards: 3
-    },{
-      epoch: "2100",
+    },
+    "2100": {
+      counting: false,
       turns: 5,
-      counting: false,
       cards: 2
-    },{
-      epoch: "2200",
-      turns: 10,
+    },
+    "2200": {
       counting: false,
+      turns: 10,
       cards: 1
     }
-  ];
+  };
 
-  $scope.board = [
-    {
-      name: "player1",
-      stream: _.merge({}, streamStructure)
-    },{
-      name: "player2",
-      stream: _.merge({}, streamStructure)
-    }
-  ];
+  $scope.board = {
+    player1: _.merge({}, streamStructure),
+    player2: _.merge({}, streamStructure)
+  };
 
-  $scope.agentDisintegrating = function (index, player) {
-    console.log(index, player);
+  $scope.agentDisintegrating = function (epoch, player) {
+    console.log(epoch, player);
 
-    $scope.board[player].stream[index].turns -= 1;
+    var influencedTurnpoint = $scope.board[player][epoch];
+
+    influencedTurnpoint.turns -= 1;
   }
 });
