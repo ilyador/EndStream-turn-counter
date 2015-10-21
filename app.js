0efe9cd75fe0e5c26a3f919274df89ba18986a9d
ilyador <whitestar.i8@gmail.com>
2015-10-21 14:57:34 +0300

    local storage and some refactoring

--- app.js
+++ app.js
@@ -1,50 +1,75 @@
-angular.module('esCounter', []).controller('boardController', function($scope) {
+var endStreamCounter = angular.module('endStreamCounter', []);
 
-  var streamStructure = {
-    "1800": {
-      isCounting: false,
-      turns: 2,
-      cards: 5,
-      score: 2
-    },
-    "1900": {
-      isCounting: false,
-      turns: 3,
-      cards: 4,
-      score: 3
-    },
-    "2000": {
-      isCounting: false,
-      turns: 4,
-      cards: 3,
-      score: 4
-    },
-    "2100": {
-      isCounting: false,
-      turns: 5,
-      cards: 2,
-      score: 6
-    },
-    "2200": {
-      isCounting: false,
-      turns: 10,
-      cards: 1,
-      score: 12
+
+endStreamCounter.constant('streamStructure', {
+  "1800": {
+    isCounting: false,
+    turns: 2,
+    cards: 5,
+    score: 2
+  },
+  "1900": {
+    isCounting: false,
+    turns: 3,
+    cards: 4,
+    score: 3
+  },
+  "2000": {
+    isCounting: false,
+    turns: 4,
+    cards: 3,
+    score: 4
+  },
+  "2100": {
+    isCounting: false,
+    turns: 5,
+    cards: 2,
+    score: 6
+  },
+  "2200": {
+    isCounting: false,
+    turns: 10,
+    cards: 1,
+    score: 12
+  }
+});
+
+
+endStreamCounter.controller('boardController', function($scope, streamStructure) {
+
+  function makeBoard() {
+    return {
+      player1: {
+        stream: _.merge({}, streamStructure),
+        score: 0
+      },
+      player2: {
+        stream: _.merge({}, streamStructure),
+        score: 0
+      }
     }
-  };
+  }
+
+  function reduceTurns(turnpoint, epoch) {
+    if(turnpoint.isCounting === $scope.nextPlayer) {
+      if (turnpoint.turns > 1) {
+        turnpoint.turns -= 1;
+      } else if (turnpoint.turns === 1) {
+        turnpoint.turns = "Disintegrate or spin"
+      } else {
+        turnpoint.turns = streamStructure[epoch].turns;
+      }
+    }
+  }
+
+  function swapPlayers() {
+    $scope.nextPlayer = $scope.currentPlayer;
+    $scope.currentPlayer = ($scope.currentPlayer === "player1") ? "player2" : "player1";
+  }
 
   $scope.currentPlayer = "player1";
-
-  $scope.board = {
-    player1: {
-      stream: _.merge({}, streamStructure),
-      score: 0
-    },
-    player2: {
-      stream: _.merge({}, streamStructure),
-      score: 0
-    }
-  };
+  $scope.nextPlayer = "player2";
+  $scope.board = (localStorage.board) ? JSON.parse(localStorage.board) : makeBoard();
 
   $scope.agentDisintegrating = function (epoch, player) {
     var turnpoint = $scope.board[player].stream[epoch];
@@ -52,24 +77,9 @@ angular.module('esCounter', []).controller('boardController', function($scope) {
   };
 
   $scope.endTurn = function () {
-    var currentPlayer = $scope.currentPlayer;
-    var nextPlayer = ($scope.currentPlayer === "player1") ? "player2" : "player1";
-
-    var reduceTurns = function(turnpoint, epoch) {
-      if(turnpoint.isCounting === nextPlayer) {
-        if (turnpoint.turns > 1) {
-          turnpoint.turns -= 1;
-        } else if (turnpoint.turns === 1) {
-          turnpoint.turns = "Disintegrate or spin"
-        } else {
-          turnpoint.turns = streamStructure[epoch].turns;
-        }
-      }
-    };
-
-    _.forOwn($scope.board[currentPlayer].stream, reduceTurns);
-    _.forOwn($scope.board[nextPlayer].stream, reduceTurns);
-
-    $scope.currentPlayer = nextPlayer;
+    _.forOwn($scope.board[$scope.currentPlayer].stream, reduceTurns);
+    _.forOwn($scope.board[$scope.nextPlayer].stream, reduceTurns);
+    swapPlayers();
+    localStorage.board = JSON.stringify($scope.board)
   }
 });
